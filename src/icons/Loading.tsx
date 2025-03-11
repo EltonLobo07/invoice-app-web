05966e98059282af295fae2b311e6f40685c05a6
Elton Lobo <eltonsw00@gmail.com>
2025-03-11 15:47:41 +0530

    move icon associated with `Spinner` to icons folder

---
+++ src/icons/Loading.tsx
@@ -0,0 +1,23 @@
+type Props = {
+  className?: string;
+};
+
+export function Loading(props: Props) {
+  return (
+    <svg
+      aria-hidden={true}
+      xmlns="http://www.w3.org/2000/svg"
+      width="14"
+      height="14"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      className={props.className}
+      viewBox="0 0 24 24"
+    >
+      <path d="M12 2v4M12 18v4M4.93 4.93l2.83 2.83M16.24 16.24l2.83 2.83M2 12h4M18 12h4M4.93 19.07l2.83-2.83M16.24 7.76l2.83-2.83"></path>
+    </svg>
+  );
+}
--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,3 +1,4 @@
 export { XOctagon } from "./XOctagon";
 export { XCircle } from "./XCircle";
 export { CheckCircle } from "./CheckCircle";
+export { Loading } from "./Loading";
