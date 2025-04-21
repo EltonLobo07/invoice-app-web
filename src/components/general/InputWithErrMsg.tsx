ddbb8628907bfed52e442f5d7c036de41bd70186
Elton Lobo <eltonsw00@gmail.com>
2025-04-21 13:31:18 +0530

    improve input error message ui

--- src/components/general/InputWithErrMsg.tsx
+++ src/components/general/InputWithErrMsg.tsx
@@ -3,6 +3,7 @@
 import { classJoin, CustomProps, OmitKey } from "@/utils/general";
 import React from "react";
 import { Input } from "./Input";
+import { AnimatePresence, motion } from "motion/react";
 
 type Props = OmitKey<
   React.ComponentPropsWithRef<typeof Input>,
@@ -16,24 +17,74 @@ export function InputWithErrMsg({ $errorMsg, ...inputProps }: Props) {
   const id = React.useId();
   const errorMsgId = `${id}-error`;
   const isInvalid = Boolean($errorMsg);
+  const [inputFocused, setInputFocused] = React.useState(false);
+  const inputRef = React.useRef<HTMLInputElement | null>(null);
+
+  React.useLayoutEffect(() => {
+    if (document.activeElement === inputRef.current) {
+      setInputFocused(true);
+    }
+  }, []);
 
   return (
-    <div className="flex flex-col gap-y-1">
+    <div
+      className={classJoin("relative", "max-w-full", "flex flex-col gap-y-1")}
+    >
       <Input
         {...inputProps}
+        ref={(ele) => {
+          inputRef.current = ele;
+          if (typeof inputProps.ref === "function") {
+            inputProps.ref(ele);
+          } else if (inputProps.ref && typeof inputProps.ref === "object") {
+            inputProps.ref.current = ele;
+          }
+        }}
+        onFocus={(e) => {
+          setInputFocused(true);
+          inputProps.onFocus?.(e);
+        }}
+        onBlur={(e) => {
+          setInputFocused(false);
+          inputProps.onBlur?.(e);
+        }}
         aria-invalid={isInvalid}
         aria-describedby={errorMsgId}
       />
       <span
         id={errorMsgId}
         className={classJoin(
-          "typography-body-sm text-ds-9",
+          "text-ds-9",
+          "typography-body-sm min-h-[var(--leading-body-sm)]",
           "inline-block transition-opacity duration-300",
+          "max-w-full overflow-x-hidden text-nowrap text-ellipsis",
           isInvalid ? "opacity-100" : "opacity-0"
         )}
       >
         {$errorMsg}
       </span>
+      <AnimatePresence>
+        {inputFocused && isInvalid && (
+          <motion.span
+            initial={{ opacity: 0, scale: 0.95 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0.95 }}
+            aria-hidden={true}
+            className={classJoin(
+              "absolute top-[calc(100%-var(--leading-body-sm))] left-0 right-0",
+              "bg-red-500 text-white",
+              "px-2 py-0.5",
+              "text-body-sm",
+              "rounded-sm",
+              "origin-[--popover-transform-origin]",
+              "max-w-full overflow-x-hidden break-words hyphens-auto",
+              "shadow-sm"
+            )}
+          >
+            {$errorMsg}
+          </motion.span>
+        )}
+      </AnimatePresence>
     </div>
   );
 }
