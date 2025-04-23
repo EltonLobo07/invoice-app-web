92165a76fd3823e4ea66b8731f5470d5315616ff
Elton Lobo <eltonsw00@gmail.com>
2025-04-23 08:43:04 +0530

    set number of invoices per page to 10 (home)

--- src/constants/home.ts
+++ src/constants/home.ts
@@ -3,5 +3,5 @@ import { InvoiceStatus } from "@/types/home";
 export const INVOICE_STATUSES: InvoiceStatus[] = ["draft", "pending", "paid"];
 export const STATUSES_SEARCH_PARAM = "statuses";
 export const PAGE_NUM_SEARCH_PARAM = "pageNum";
-export const NUM_INVOICES_PER_PAGE = 5;
+export const NUM_INVOICES_PER_PAGE = 10;
 export const CREATE_INVOICE_SEARCH_PARAM = "createInvoice";
